bd21746cd98d5cb61a497eeedaccc2cf1b64bac9
Natalie J. Bennett <nbennett@pivotal.io>
2018-04-01 13:13:54 -0700

    Initial scaffolding

---
+++ pop/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("ID: 0 Sex: M Parents: - -")
+	fmt.Println("ID: 1 Sex: M Parents: - -")
+	fmt.Println("ID: 2 Sex: M Parents: 1 2")
+	fmt.Println("ID: 3 Sex: M Parents: 1 2")
+}
