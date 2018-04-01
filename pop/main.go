960742b85f61116fee0a296a10aa4a804b56d9c8
Natalie J. Bennett <nbennett@pivotal.io>
2018-04-01 13:53:50 -0700

    Represent sim values from a struct

--- pop/main.go
+++ pop/main.go
@@ -2,10 +2,25 @@ package main
 
 import "fmt"
 
-func main() {
+type sim struct {
+	id      int
+	sex     string
+	parent1 *sim
+	parent2 *sim
+}
 
-	fmt.Println("ID: 0 Sex: M Parents: - -")
-	fmt.Println("ID: 1 Sex: M Parents: - -")
+func (s sim) format() string {
+	return fmt.Sprintf("ID: %d Sex: %s Parents: - -", s.id, s.sex)
+}
+
+func main() {
+	sims := []sim{
+		sim{id: 0, sex: "M"},
+		sim{id: 1, sex: "F"},
+	}
+
+	fmt.Println(sims[0].format())
+	fmt.Println(sims[1].format())
 	fmt.Println("ID: 2 Sex: M Parents: 1 2")
-	fmt.Println("ID: 3 Sex: M Parents: 1 2")
+	fmt.Println("ID: 3 Sex: F Parents: 1 2")
 }
