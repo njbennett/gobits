d6c2277f597c4b7901f90cdbee0058bc044bc90c
Natalie J. Bennett <nbennett@pivotal.io>
2018-04-01 18:39:23 -0700

    Introduce time

--- pop/main.go
+++ pop/main.go
@@ -7,10 +7,11 @@ type sim struct {
 	sex     int
 	parent1 *sim
 	parent2 *sim
+	born    int
 }
 
 func (s sim) format() string {
-	return fmt.Sprintf("ID: %d Sex: %d Parents: %s", s.id, s.sex, s.parents())
+	return fmt.Sprintf("ID: %d Born: %d Sex: %d Parents: %s", s.id, s.born, s.sex, s.parents())
 }
 
 func (s sim) parents() string {
@@ -21,23 +22,25 @@ func (s sim) parents() string {
 	return fmt.Sprintf("%d %d", s.parent1.id, s.parent2.id)
 }
 
-func newSim(s []sim) sim {
-	return sim{
+func newSim(s []sim, year int) (error, sim) {
+	return nil, sim{
 		id:      len(s),
 		sex:     len(s) % 2,
 		parent1: &s[0],
 		parent2: &s[1],
+		born:    year,
 	}
 }
 
 func main() {
 	sims := []sim{
-		sim{id: 0, sex: 0},
-		sim{id: 1, sex: 1},
+		sim{id: 0, sex: 0, born: 0},
+		sim{id: 1, sex: 1, born: 0},
 	}
 
-	for i := 0; i < 100; i++ {
-		sims = append(sims, newSim(sims))
+	for i := 20; i < 100; i++ {
+		_, sim := newSim(sims, i)
+		sims = append(sims, sim)
 	}
 
 	for _, s := range sims {
